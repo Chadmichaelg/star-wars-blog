400ff4ae2e4dd903b824933eb15f3e37684e135b
Chadmichaelg <chadmgrubbs@gmail.com>
2022-04-28 00:04:00 +0000

    initial commit

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -46,3 +46,5 @@ const injectContext = PassedComponent => {
 };
 
 export default injectContext;
+
+// injectContext
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,19 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import { Characters } from "./Characters";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	
+	return (
+		<div>
+			<h1>Characters</h1>
+<Characters />
+
+			<h1>Planets</h1>
+		</div>
+		)
+	};
+		
+	
+
